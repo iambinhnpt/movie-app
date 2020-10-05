469caa45c9bd9f2760029ea30a7138882f054b19
iambinhnpt <iambinhnpt@gmail.com>
2020-10-05 23:58:38 +0700

    add commit

--- src/App.js
+++ src/App.js
@@ -1,57 +1,114 @@
 import React, { useEffect, useState } from "react";
 import Movie from "./components/Movie";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider, Query, useQuery } from "react-apollo";
+import gql from "graphql-tag";
 
-const FEATURED_API =
-  "https://api.themoviedb.org/3/discover/movie?api_key=21a033efad5bde562a70333aacb2dce5&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1";
+const client = new ApolloClient({
+  uri: "https://ion-movies.herokuapp.com/",
+});
 
-const SEARCH_API =
-  "https://api.themoviedb.org/3/search/movie?api_key=21a033efad5bde562a70333aacb2dce5&query=";
+const NOW_QUERY = gql`
+  query {
+    nowPlaying {
+      count
+      page
+      movies {
+        id
+        title
+        original_title
+        original_language
+        overview
+        video
+        poster_path
+        backdrop_path
+        popularity
+        adult
+        vote_count
+        vote_average
+        release_date
+      }
+    }
+  }
+`;
+
+const DETAIL_QUERY = gql`
+  query {
+    details(movieId: 337401) {
+      id
+      title
+      original_title
+      original_language
+      overview
+      video
+      poster_path
+      backdrop_path
+      popularity
+      adult
+      vote_count
+      vote_average
+      release_date
+    }
+  }
+`;
 
 function App() {
+  const { data } = useQuery({ query: NOW_QUERY });
   const [movies, setMovie] = useState([]);
-  const [searchTerm, setSearchTerm] = useState("");
 
   useEffect(() => {
     fetch(FEATURED_API)
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
         setMovie(data.results);
       });
   }, []);
 
-  const handleOnSubmit = (e) => {
-    e.preventDefault();
-    fetch(SEARCH_API + searchTerm)
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data);
-        setMovie(data.results);
-      });
-  };
-
-  const handleOnChange = (e) => {
-    setSearchTerm(e.target.value);
-  };
-
   return (
-    <>
+    <ApolloProvider client={client}>
       <header>
-        <form onSubmit={handleOnSubmit}>
-          <input
-            className="search"
-            type="text"
-            placeholder="Search..."
-            onChange={handleOnChange}
-          />
-        </form>
+        <h1>The Movies</h1>
       </header>
-      <div className="movie-container">
-        {movies.map((movie) => (
+
+      {/* {movies.map((movie) => (
           <Movie key={movie.id} {...movie} />
-        ))}
-      </div>
-    </>
+        ))} */}
+
+      <Query query={NOW_QUERY}>
+        {({ loading, error, data, fetchMore }) => {
+          if (loading) return "Loading...";
+          if (error) return "Error";
+          console.log(data.nowPlaying.movies);
+          return (
+            <>
+              <div className="movie-container">
+                {data.nowPlaying.movies.map((item) => (
+                  <Movie key={item.id} {...item} />
+                ))}
+              </div>
+              <button
+                onClick={() => {
+                  const { endCursor } = data.nowPlaying.page;
+
+                  fetchMore({
+                    variable: { after: endCursor },
+                    updateQuery: (prevResult, { fetchMoreResult }) => {
+                      fetchMoreResult.nowPlaying.movies = [
+                        ...prevResult.nowPlaying.movies,
+                        ...fetchMoreResult.nowPlaying.movies,
+                      ];
+                      return fetchMoreResult;
+                    },
+                  });
+                }}
+              >
+                More
+              </button>
+            </>
+          );
+        }}
+      </Query>
+    </ApolloProvider>
   );
 }
 
